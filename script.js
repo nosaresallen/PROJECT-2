a60974376991a7e5179d9f19f4200cd244ba3f50
nosaresallen <nosaresallen@outlook.com>
2023-09-29 01:47:33 +0800

    added a about modal and few changes in styles

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ let bgmGameOver = document.querySelector('#gameover');
 let guessCount = 10; // guess counter
 
 
-const audio = document.querySelector('#bgMusic');
 
+//backgorund music
+const audio = document.querySelector('#bgMusic');
 function toggleAudio() {
     if (audio.muted) {
         audio.muted = false;
@@ -23,6 +24,8 @@ function toggleAudio() {
 }
 
 
+
+
 guessBtn.addEventListener('click', () => {
     //This is Modal --To display 
     const congratsModal = new bootstrap.Modal(document.getElementById('congratsModal'))
@@ -30,51 +33,62 @@ guessBtn.addEventListener('click', () => {
     const messageModal2 = new bootstrap.Modal(document.getElementById('messageModal2'))
     const gameOverModal = new bootstrap.Modal(document.getElementById('gameOverModal'))
     const invalidModal = new bootstrap.Modal(document.getElementById('invalidModal'))
-
     
     //user input number
     let input = parseFloat(document.querySelector('#userInput').value); 
     
-    // Add number validation
+    //input validation
     if (isNaN(input) || input < 0 || input > 100) {
         invalidModal.show();
         document.querySelector('#userInput').value = ''; 
         return; 
     }
 
+    //display the congratlations modal if user input the correct number
+    //play the victory sound
     if (input == number){
         document.querySelector('#correctNumber').innerHTML = `${number}`;
         congratsModal.show();
         bgmVictory.play();
         bgMusic.pause();
 
-
+    //display too high modal message
+    //decrease guess remaining
     } if (input > number) {
         messageModal1.show();
         document.querySelector('#userInput').value = '';
         guessCount--;
 
+    //display too low modal message
+    //decrease guess remaining
     } if (input < number) {
         messageModal2.show();
         document.querySelector('#userInput').value = '';
         guessCount--;
     }
+
+    //diplay the game over modal if the guess remaining reached 0
     if (guessCount === 0) {
         gameOverModal.show();
         bgmGameOver.play();
         bgMusic.pause();
     }
-
-    
-    
-
     
     updateGuess(); //updating a guess counter
 });
 
 
 
+//about game modal --to display
+document.querySelector('#aboutBtn').addEventListener('click', aboutFunction);
 
+function aboutFunction(){
+    const aboutModal = new bootstrap.Modal(document.getElementById('aboutModal'))
+    aboutModal.show();
+}
+
+
+//guess remaining 
 function updateGuess(){
     document.querySelector('#guessCount').innerHTML = guessCount;
 }
